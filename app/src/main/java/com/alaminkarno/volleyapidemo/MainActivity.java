40d34dd24a8fc9fb2cbb89c64ff476682cbc7506
User <https://github.com/karno786/Sqlite_git_test>
2020-07-28 02:23:49 +0600

    GET Single String 500 Line.

--- app/src/main/java/com/alaminkarno/volleyapidemo/MainActivity.java
+++ app/src/main/java/com/alaminkarno/volleyapidemo/MainActivity.java
@@ -11,6 +11,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -19,13 +20,38 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RequestQueue requestQueue;
+    private String URL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String URL = "https://jsonplaceholder.typicode.com/todos/1";
+        requestQueue = Volley.newRequestQueue(this);
+        URL = "https://www.google.com";
+
+        JSONStringRequest();
+
+       // JSONObjectRequest();
+    }
+
+    private void JSONStringRequest() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, "Response is: "+response.substring(0,500), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Error: "+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    private void JSONObjectRequest() {
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
             @Override
@@ -42,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error: Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
-
         requestQueue.add(jsonObjectRequest);
+
     }
 }
